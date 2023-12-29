bbe02c5a15ebff7fdcb4c342592194af81e87c9b
Ida Whitcomb <idawhitcomb@gmail.com>
2023-12-29 09:06:10 -0500

    appended the api routes so that existing notes would be displayed and new notes could be added.

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -2,20 +2,21 @@
 const api = require('express').Router();
 const fs = require('fs');
 const path = require('path');
+const database = require ('../db/notes.json');
 
 //Import helper functions for reading and writing to JSON file.
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
 
 
 //const { v4: uuidv4 } = require('uuid');
 
 //GET Route to retrieve all existing notes
-api.get('api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '..Develop/db/notes.json'));
+api.get('/api/notes', (req, res) => {
+    readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
 });
 
 //POST route for inputting new note
-api.post('api/notes', (req, res) => {
+api.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to submit feedback`);
     const{ title , text } = req.body;
 
@@ -24,18 +25,18 @@ api.post('api/notes', (req, res) => {
             title,
             text
     };
-       
+
     readAndAppend(newNote, './db/notes.json');
 
-    const response = {
+/*    const response = {
         status: 'success',
         body: newNote,
-      };
+      };*/
   
       res.json(response);
 
 } else {
-    res.json('Error in posting feedback');
+    res.json('Error in posting note');
 };
 });
 
