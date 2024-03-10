e28bb21476a84d16fd70751bf2a9a33c49b5c4e1
Ida Whitcomb <idawhitcomb@gmail.com>
2024-03-10 13:51:50 -0400

    Moved files around and began on README file

--- Develop/routes/api_routes.js
+++ Develop/routes/api_routes.js
@@ -2,13 +2,10 @@
 const api = require('express').Router();
 const fs = require('fs');
 const path = require('path');
-const database = require ('../db/notes.json');
 
 //Import helper functions for reading and writing to JSON file.
 const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
-
-
-//const { v4: uuidv4 } = require('uuid');
+const uuid = require('../helpers/uuid')
 
 //GET Route to retrieve all existing notes
 api.get('/api/notes', (req, res) => {
@@ -21,7 +18,7 @@ api.get('/notes/:id', (req, res) => {
     readFromFile('./db/notes.json')
         .then((data) => JSON.parse(data))
         .then((json) => {
-            const result = json.filter((note) => note.noteId === noteId);
+            const result = json.filter((note) => note.id === noteId);
             return result.length > 0
             ? res.json(result)
             : res.json('No note found with that ID');
@@ -31,20 +28,22 @@ api.get('/notes/:id', (req, res) => {
 //POST route for inputting new note
 api.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to submit feedback`);
-    const{ title , text } = req.body;
+
+    const { title, text } = req.body;
 
     if(title && text) {
         const newNote = {
-            title,
-            text
+            title: title,
+            text: text,
+            id: uuid(),
     };
 
     readAndAppend(newNote, './db/notes.json');
 
-/*    const response = {
+    const response = {
         status: 'success',
         body: newNote,
-      };*/
+      };
   
       res.json(response);
 
@@ -53,23 +52,24 @@ api.post('/api/notes', (req, res) => {
 };
 });
 
-//DELETE route to delete specific notes
-api.delete('/notes/:id', (req, res) => {
-    const noteId = req.params.id;
+// //DELETE route to delete specific notes
+// api.delete('api/notes/:id', (req, res) => {
+//     const noteId = req.params.id;
 
-    readFromFile('./db/notes.json')
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-        const results = json.filter((note) => note.id !== noteId);
-        writeToFile('./db/notes.json', results);
-    }) .then (() => {
-        res.json('Note successfully deleted');
-    })
-    .catch((err) => {
-        console.error(err);
-        res.status(500).json({ error: 'Failed to delete note' });
-    });
-});
+//     readFromFile('./db/notes.json')
+//     .then((data) => JSON.parse(data))
+//     .then((json) => {
+//         const results = json.filter((note) => note.id !== noteId);
+//         return writeToFile('./db/notes.json', results);
+//     })
+//     .then (() => {
+//         res.json('Note successfully deleted');
+//     })
+//     .catch((err) => {
+//         console.error(err);
+//         res.status(500).json({ error: 'Failed to delete note' });
+//     });
+// });
 
 
 //Export router
--- README.md
+++ README.md
@@ -1,16 +1,20 @@
 ## Note Taking Application
 
 ## Description
+This is a simple note taking application. Users who access this application can write their notes in the right-hand side of the screen and save them to a local database.
 
 ## Badges
 N/A
 
 ## Visuals
-
+<img src='./Photos/notes_page.png'>
+Deployed Application: 
 
 ## Installation
+This application is already deployed to heroku. To access it the user simply needs to click on the deployed application link above.
 
 ## Usage
+
 
 ## Contributing
 N/A
@@ -22,4 +26,4 @@ This program was written by myself, Ida Whitcomb, with starter code supplied by 
 N/A
 
 ## Project status
-This project is complete.
+This project is still in progress. It still needs a working delete route so that users can delete notes they no longer need.
